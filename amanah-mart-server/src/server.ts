216859bf0bf1fa51abda58305c4d9a660868a17c
Hasan9955 <hasanbinali7556@gmail.com>
2024-12-15 21:01:13 +0600

    Rename the file

--- amanah-mart-server/src/server.ts
+++ amanah-mart-server/src/server.ts
@@ -3,6 +3,8 @@ import app from "./app";
 import config from "./app/config";
 
 
+
+
 async function main() {
     const server: Server = app.listen(config.port, () => {
         console.log(`Amanah Mart server is running on port: ${config.port}`)
